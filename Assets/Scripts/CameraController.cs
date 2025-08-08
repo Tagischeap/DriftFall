50fedadd3e2dea20e1925cfa6485e504e34098c7
Taggy <Tagischeap@Gmail.com>
2025-08-08 07:50:10 -0400

    Basic Controls
    Following some simple tutorial to get something going.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public float followSpeed;
+    public float rotateSpeed;
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position, followSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation, target.rotation, rotateSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CarController : MonoBehaviour
+{
+    public float acceleration;
+    public float turnSpeed;
+
+    public Transform carModel;
+    public Rigidbody rig;
+    private Vector3 startModelOffset;
+    public float groundCheckRate;
+    private float lastGroundCheckTime;
+
+    private float curYRot;
+    private bool accelerateInput;
+    private float turnInput;
+
+    void Start()
+    {
+        startModelOffset = carModel.transform.localPosition;
+    }
+    void Update()
+    {
+        curYRot += turnInput * turnSpeed * Time.deltaTime;
+
+        carModel.transform.position = transform.position + startModelOffset;
+        carModel.transform.eulerAngles = new Vector3(0, curYRot, 0);
+    }
+    void FixedUpdate()
+    {
+        if (accelerateInput)
+        {
+            rig.AddForce(carModel.forward * acceleration, ForceMode.Acceleration);
+        }
+    }
+    public void OnAccelerateInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+            accelerateInput = true;
+        else
+            accelerateInput = false;
+    }
+
+    public void OnTurnInput(InputAction.CallbackContext context)
+    {
+        turnInput = context.ReadValue<float>();
+    }
+}
